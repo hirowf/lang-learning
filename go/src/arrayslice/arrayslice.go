1ba8761dcc0e95b000c13948388dedae72bdc96b
hirowf <hiroto.anderson@gmail.com>
2025-04-08 19:15:16 -0300

    arrays and slices

---
+++ go/src/arrayslice/arrayslice.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Arrays and Slices")
+
+	var arr [5]string
+	arr[0] = "position 1"
+	fmt.Println(arr)
+
+	arr2 := [5]int{0, 1, 3, 4, 5}
+	// arr2[5] = 2
+	fmt.Println(arr2)
+
+	arr3 := [...]int{1, 2, 3, 4, 5, 6, 7}
+
+	fmt.Println(arr3)
+
+	slice := []int{10, 2032, 23123, 9493, 30032, 03012, 0300, 332342, 2321321}
+
+	fmt.Println(slice)
+	// fmt.Println(reflect.TypeOf(slice))
+	// fmt.Println(reflect.TypeOf(arr3))
+
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	slice2 := arr2[1:5]
+	fmt.Println(slice2)
+	arr2[1] = 34
+	fmt.Println(slice2)
+}
